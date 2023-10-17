d949efaade5c43ce53cc18aaee47c6d6fb1f51e1
Mehmet Can Erturk <cannhann42@gmail.com>
2023-10-17 23:12:02 +0300

    Update the user model used for MongoDB
    - Add a new field to the user model called 'name'

--- src/lib/mongodb/models/user.jsx
+++ src/lib/mongodb/models/user.jsx
@@ -7,6 +7,10 @@ const userSchema = new Schema(
       required: true,
       default: "credentials",
     },
+    name: {
+      type: String,
+      required: true,
+    },
     email: {
       type: String,
       required: true,
