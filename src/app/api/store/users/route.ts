393733c68ac82e87b1515f9fbd198467968cf25b
Can Erturk <cannhann42@gmail.com>
2024-04-29 21:33:38 +0300

    Create `store/users` endpoint - to get user information by given id

---
+++ src/app/api/store/users/route.ts
@@ -0,0 +1,46 @@
+import connectDB from '@/config/mongodb/connectDB';
+import User from '@models/user.model';
+import { isValidObjectId } from 'mongoose';
+import response from '@helpers/response';
+import { NextRequest } from 'next/server';
+import { UserType } from '@/types/mongoose/user.type';
+
+export async function GET(req: NextRequest) {
+  const { searchParams }: URL = new URL(req.url);
+  const _id: string | null = searchParams.get('user_id');
+
+  if (!_id) {
+    return response({
+      status: 400,
+      message: 'User ID is required.',
+    });
+  }
+
+  // Check if _id is not a valid ObjectId
+  if (!isValidObjectId(_id)) {
+    return response({
+      status: 400,
+      message: 'Invalid user ID.',
+    });
+  }
+
+  try {
+    await connectDB();
+    const user: UserType | null = await User.findOne({ _id });
+
+    if (user) {
+      return response({ status: 200, data: user });
+    } else {
+      return response({ status: 404, message: 'User not found.' });
+    }
+
+    // Catch any errors
+  } catch (error) {
+    console.error('Search results could not be retrieved:', error);
+
+    return response({
+      status: 500,
+      message: 'An internal server error. Check server logs for details.',
+    });
+  }
+}
