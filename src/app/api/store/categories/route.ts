35437b044256747727bf1af5fbbbee7a163c6165
Can Erturk <cannhann42@gmail.com>
2024-04-29 13:22:22 +0300

    Create `store/categories` endpoint - If category_id is provided, return the category with the given id. - If path is provided, return the category with the given path. - Else, if category_id and path are not provided, return all categories.

---
+++ src/app/api/store/categories/route.ts
@@ -0,0 +1,59 @@
+import connectDB from '@/config/mongodb/connectDB';
+import Category from '@models/category.model';
+import response from '@helpers/response';
+import { NextRequest } from 'next/server';
+import { CategoryType } from '@/types/mongoose/category.type';
+
+export async function GET(req: NextRequest) {
+  const { searchParams }: URL = new URL(req.url);
+  const categoryID: string | null = searchParams.get('id');
+  const path: string | null = searchParams.get('path');
+
+  try {
+    await connectDB();
+
+    // Find and return category by id
+    if (categoryID) {
+      const category: CategoryType | null = await Category.findOne({
+        category_id: categoryID,
+      });
+
+      if (category) {
+        return response({ status: 200, data: category });
+      } else {
+        return response({ status: 404, message: 'Category not found.' });
+      }
+    }
+
+    // Find and return category by url path
+    if (path) {
+      const category: CategoryType | null = await Category.findOne({
+        path,
+      });
+
+      if (category) {
+        return response({ status: 200, data: category });
+      } else {
+        return response({ status: 404, message: 'Category not found.' });
+      }
+    }
+
+    // Return all categories
+    const categories: CategoryType[] | null = await Category.find({});
+
+    if (categories) {
+      return response({ status: 200, data: categories });
+    } else {
+      return response({ status: 404, message: 'No categories found.' });
+    }
+
+    // Catch any errors
+  } catch (error) {
+    console.error('Search results could not be retrieved:', error);
+
+    return response({
+      status: 500,
+      message: 'An internal server error. Check server logs for details.',
+    });
+  }
+}
