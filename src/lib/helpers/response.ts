e46079af99013900ebf3ba7b904f49eb0d397263
Can Erturk <cannhann42@gmail.com>
2024-05-01 23:43:06 +0300

    Update the helper function named `response` - Use `ok: true|false` instead of `status: OK|ERR` - Success condition is now between 100-226 instead of 200-299

--- src/lib/helpers/response.ts
+++ src/lib/helpers/response.ts
@@ -11,9 +11,7 @@ export default function response({
   message,
   data,
 }: ResponseParams): NextResponse {
-  // Create a status text based on the status code
-  let statusText: string = status >= 200 && status < 300 ? 'OK' : 'ERR';
+  let ok: Boolean = status >= 100 && status <= 226 ? true : false;
 
-  // Return the response with the given information
-  return NextResponse.json({ status, statusText, message, data });
+  return NextResponse.json({ ok, status, message, data });
 }
